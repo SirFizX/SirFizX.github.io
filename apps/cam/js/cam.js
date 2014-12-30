abf1702a4ffab90bdaa8dc70c21c424e842da761
SirFizX <eric.eisaman@live.com>
2014-12-30 18:01:54 -0500

    up

--- apps/cam/js/cam.js
+++ apps/cam/js/cam.js
@@ -26,6 +26,6 @@ if(navigator.getUserMedia) { // Standard
 
 // Trigger photo take
   document.getElementById("snap").addEventListener("click", function() {
-  context.drawImage(video, 0, 0, 640, 480);
+  context.drawImage(video, 0, 0, 800, 512);
   });
   }, false);
